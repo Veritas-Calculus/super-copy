f2fc1a6fe6c135c86b11002468da25e345947666
Chris Su <chris@lesscrowds.org>
2026-01-19 15:54:58 +0800

    fix(security): Force secure versions of transitive dependencies
    - Netty 4.1.118.Final (CVE-2023-44487, CVE-2024-29025, etc.)
    - Guava 33.4.0-jre (CVE-2023-2976)
    - Protobuf 4.29.3 (CVE-2024-7254)
    - Commons Compress 1.27.1 (CVE-2024-25710, CVE-2024-26308)
    - JDOM2 2.0.6.1 (XXE vulnerability)
    - jose4j 0.9.7 (DoS vulnerability)
    
    Fixes Dependabot alerts #1-17

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,32 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+// Force secure versions of transitive dependencies to fix vulnerability alerts
+subprojects {
+    configurations.all {
+        resolutionStrategy {
+            // Netty vulnerabilities (CVE-2023-44487, CVE-2024-29025, etc.)
+            force("io.netty:netty-codec:4.1.118.Final")
+            force("io.netty:netty-codec-http:4.1.118.Final")
+            force("io.netty:netty-codec-http2:4.1.118.Final")
+            force("io.netty:netty-handler:4.1.118.Final")
+            force("io.netty:netty-common:4.1.118.Final")
+            
+            // Guava vulnerability (CVE-2023-2976)
+            force("com.google.guava:guava:33.4.0-jre")
+            
+            // Protobuf vulnerability (CVE-2024-7254)
+            force("com.google.protobuf:protobuf-java:4.29.3")
+            
+            // Commons Compress vulnerabilities
+            force("org.apache.commons:commons-compress:1.27.1")
+            
+            // JDOM2 XXE vulnerability
+            force("org.jdom:jdom2:2.0.6.1")
+            
+            // jose4j DoS vulnerability
+            force("org.bitbucket.b_c:jose4j:0.9.7")
+        }
+    }
+}
